b710e7852f936f056f05cf65e8df9f8e175e4059
simona thrussell <sdthrussell@gmail.com>
2025-05-15 14:22:56 +0200

    template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
-    #[Route('/ Sales', name: 'app_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('index/index.html.twig', [
