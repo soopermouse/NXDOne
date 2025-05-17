7d2814074070cce38c0945c799e079981830df1c
simona thrussell <sdthrussell@gmail.com>
2025-05-17 14:07:23 +0200

    permissions

---
+++ src/Entity/Permission.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PermissionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PermissionRepository::class)]
+class Permission
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $PermissionId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $Description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPermissionId(): ?int
+    {
+        return $this->PermissionId;
+    }
+
+    public function setPermissionId(int $PermissionId): static
+    {
+        $this->PermissionId = $PermissionId;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+}
